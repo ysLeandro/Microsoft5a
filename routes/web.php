205495da2a18d3e0676d0fab46936132209e4b86
STALIN FRANCIS <educaysoft@educaysoft.org>
2025-04-29 14:39:25 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ session_start();
 require_once '../app/controllers/SexoController.php';
 
 $requestUri = $_SERVER["REQUEST_URI"];
-$basePath = '/eysphp/public/';
+$basePath = '/Microsoft5a/public/';
 // Remover el prefijo basePath
 $route = str_replace($basePath, '', $requestUri);
 $route = strtok($route, '?'); // Quitar parámetros GET
